2c3df48d39ab99be1eb4f56d25f735629b849509
Christopher Henderson <chenderson5280@gmail.com>
2020-10-02 19:13:19 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,7 +1,7 @@
 # don't forget to add: require 'pry'
 
 def generate_star_date
-   star_date  = (rand(100000) + 400000) / 10.0 
+   star_date  = (rand(100000) + 400000) / 10.0
 end
 
 
@@ -12,5 +12,5 @@ end
 def engage
 date = generate_star_date
   puts state_log(date)
-  
+
 end
